b1c002c31bada79662b4a282222d32b79eebbf6c
Jeff Wachtel <jwachtel911@gmail.com>
2018-07-20 14:10:43 -0700

    Played with moving from state to state

--- CPRdemo/CPRdemo.ino
+++ CPRdemo/CPRdemo.ino
@@ -5,13 +5,17 @@
 #include <Adafruit_GFX.h>
 #include "Adafruit_LEDBackpack.h"
 #include <Wire.h>
+//#include <WaveHC.h>
+//#include <WaveUtil.h>
 
 #define POT_PIN_TIME A0 // Input from TIME pot
 #define POT_PIN_BEATSPERMINUTE A1 // Input from BEATSPERMINUTE pot
 #define LED_PIN 11 // Unused right now
-#define BUTTON_STARTSTOP_PIN 2 // buttonStartStop
-#define BUTTON_ADULTCHILD_PIN 4 // AdultChild
+#define BUTTON_STARTSTOP 2 // Set pin 2 for StartStop button
+#define BUTTON_ADULTCHILD 4 // Set pin 4 for AdultChild button
 #define NUM_SAMPLES 10
+#define LED_STARTSTOP 8 // Set pin 8 for Start/Stop button LED
+#define LED_ADULTCHILD 12 //Set pin 12 for Adult/Child button LED
 
 Adafruit_7segment redDisplay = Adafruit_7segment();
 Adafruit_7segment greenDisplay = Adafruit_7segment();
@@ -35,26 +39,38 @@ StateID currentState = SETUP;
 // variables for setup state
 int previousTimePotValues[NUM_SAMPLES];
 int currentTimePotIndex = 0;
-const int MAX_NUM_SECONDS = 90;
+const int MAX_NUM_SECONDS = 120;
 const int MIN_NUM_SECONDS = 30;
  
 // the setup function runs once when you press reset or power the board
 void setup() {
   
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(2, INPUT_PULLUP);
-
+  // initialize digital pins.
+	pinMode(LED_PIN, OUTPUT); //Unused now
+	pinMode(LED_BUILTIN, OUTPUT); //Unused now
+	pinMode(LED_STARTSTOP, OUTPUT);
+	pinMode(LED_ADULTCHILD, OUTPUT);
+	pinMode(2, INPUT_PULLUP);
+	pinMode(4, INPUT_PULLUP);
+  
   greenDisplay.begin(0x70);
   redDisplay.begin(0x71);
+
+	//Clear the displays
+  greenDisplay.clear();
+  redDisplay.clear();
+  greenDisplay.writeDisplay();
+  redDisplay.writeDisplay();
   
+  delay(2000);
   Serial.begin(9600);
 
   for (int i = 0; i < NUM_SAMPLES; i++) {
    previousTimePotValues[i] =map(analogRead(POT_PIN_TIME), 0, 1023, MIN_NUM_SECONDS, MAX_NUM_SECONDS);
 }
-}
+} //End setup 
+
+
 
 boolean CheckDebounce(int instReading, int triggerVal = LOW)
 {
@@ -85,7 +101,7 @@ boolean CheckDebounce(int instReading, int triggerVal = LOW)
 void GoToNextState(bool includeCalibration = false)
 {
   Serial.println("Old state id: " + (String)currentState);
-  int newStateId = (currentState + 1) % (includeCalibration ? 4 : 3); //What's this?: "? 4 : 3"
+  int newStateId = (currentState + 1) % (includeCalibration ? 4 : 3); 
   Serial.println("New state id: " + (String)newStateId);
   Serial.println();
   currentState = newStateId;
@@ -102,30 +118,56 @@ void UpdateSetup() {
   
   redDisplay.print((int)averageTimePotValue);
   redDisplay.writeDisplay();
-  
+
+  int instReading = digitalRead(BUTTON_STARTSTOP);
+  if(CheckDebounce(instReading)) //if switch is stable (debounced)
+  {
+    GoToNextState(true); //go to PLAY state
+  }
+
 }
 
 void UpdatePlay() {
   redDisplay.writeDigitNum(0, PLAY);
   redDisplay.writeDisplay();
+  Serial.println("Now we're in the PLAY state.") ;  
+
+  int instReading = digitalRead(BUTTON_STARTSTOP);
+  if(CheckDebounce(instReading)) //if switch is stable (debounced)
+  {
+    GoToNextState(true); //go to FEEDBACK state
+  }
+  
 }
 
 void UpdateFeedback() {
   redDisplay.writeDigitNum(0, FEEDBACK);
   redDisplay.writeDisplay();
+  Serial.println("Now we're in the FEEDBACK state.") ;
+  int instReading = digitalRead(BUTTON_STARTSTOP);
+  if(CheckDebounce(instReading)) //if switch is stable (debounced)
+  {
+    GoToNextState(true); //go to CALIBRATION state
+  }
 }
 
 void UpdateCalibration() {
   redDisplay.writeDigitNum(0, CALIBRATION);
   redDisplay.writeDisplay();
+  Serial.println("Now we're in the CALIBRATION state.") ;
+    int instReading = digitalRead(BUTTON_STARTSTOP);
+  if(CheckDebounce(instReading)) //if switch is stable (debounced)
+  {
+    GoToNextState(true); //go to SETUP state
+  }
 }
 
 // the loop function runs over and over again forever
 void loop() {
   
-  int instReading = digitalRead(BUTTON_STARTSTOP_PIN);
+  int instReading = digitalRead(BUTTON_STARTSTOP);
 
-  if(CheckDebounce(instReading))
+  if(CheckDebounce(instReading)) //if switch is stable (debounced)
   {
     GoToNextState(true);
   }
