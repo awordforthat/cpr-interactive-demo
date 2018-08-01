8518334ee9c4740fc153c154c0ba78283be9b436
Jeff Wachtel <jwachtel911@gmail.com>
2018-07-31 20:37:44 -0700

    Minor changes

--- CPRdemo/Potentiometer.cpp
+++ CPRdemo/Potentiometer.cpp
@@ -3,7 +3,7 @@
 class Potentiometer {
     int logicPinNum;
     int bufferSize;
-    int hysteresisBuffer[10]; // max size 10; by changing bufferSize you can change the effective size to something smaller
+    int hysteresisBuffer[50]; // max size 10; by changing bufferSize you can change the effective size to something smaller
     int hysteresisIndex;
     bool value;
 
